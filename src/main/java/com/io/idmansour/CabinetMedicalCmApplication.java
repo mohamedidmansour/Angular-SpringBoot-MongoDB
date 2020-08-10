9508a5bbc229446bc4c2453a290a02edf4825ab8
IDMANSOUR MOHAMED <mi.idmansour@gmail.com>
2020-08-10 14:38:32 +0200

    CM -commit

---
+++ src/main/java/com/io/idmansour/CabinetMedicalCmApplication.java
@@ -0,0 +1,13 @@
+package com.io.idmansour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetMedicalCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetMedicalCmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/idmansour/CabinetMedicalCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.idmansour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetMedicalCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
